af05bd21b5c8bae20c3d73a0586511fef4016416
alothmanmj <alothman.maj@gmail.com>
2024-02-06 14:46:44 +0300

    Adding a method to read the student list

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println(readStudent());
+    }
+
+    public static ArrayList<String> readStudent(){
+        ArrayList<String> list = new ArrayList<String>();
+        try {
+            Scanner sc =new Scanner(new File("Student List.txt"));
+            while(sc.hasNextLine()){
+                list.add(sc.nextLine());
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e.getMessage());
+        }
+
+        return list;
     }
 }
