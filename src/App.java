f25bb207314c6e26c6cd1b6cc8baf14a5a78c5d9
alothmanmj <alothman.maj@gmail.com>
2024-02-06 15:04:24 +0300

    created a method to generate random groups

--- src/App.java
+++ src/App.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 import java.io.File;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println(readStudent());
+        generateRandomGroups(readStudent(),4);
     }
 
     public static ArrayList<String> readStudent(){
@@ -20,5 +21,18 @@ public class App {
         }
 
         return list;
+    }
+
+    public static void generateRandomGroups(ArrayList<String> students,int n){
+        Collections.shuffle(students);
+        ArrayList<ArrayList<String>> groups = new ArrayList<ArrayList<String>>();
+        for(int i = 0;i<n;i++){
+            groups.add(new ArrayList<String>());
+        }
+        for (int i = 0; i< students.size();i++){
+            groups.get(i%(n)).add(students.get(i));
+        }
+        System.out.println(groups);
+
     }
 }
